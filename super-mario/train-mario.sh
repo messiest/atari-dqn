f795d89c54c7c6de091a14e7b4a5ba76580e2b3b
Chris Messier <messiercr@gmail.com>
2018-11-15 21:05:30 +0000

    corrected typo

--- super-mario/train-mario.sh
+++ super-mario/train-mario.sh
@@ -3,7 +3,7 @@
 ENVIRONMENT=$1
 
 echo Training $ENVIRONMENT model...
-xvfb-run -s "-screen 0 1400x900x24" python main.py --env-name $ENVIRONMENT --record --start_fresh
+xvfb-run -s "-screen 0 1400x900x24" python main.py --env-name $ENVIRONMENT --record --start-fresh
 
 git add checkpoints/
 git add playback/
