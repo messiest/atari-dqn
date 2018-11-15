2093ed42a7530e90794374cf40c62f8025182969
Chris <messiercr@gmail.com>
2018-11-15 16:15:20 -0500

    corrected merge artifacts

--- super-mario/train-mario.sh
+++ super-mario/train-mario.sh
@@ -3,11 +3,8 @@
 ENVIRONMENT=$1
 
 echo Training $ENVIRONMENT model...
-<<<<<<< HEAD
-env WARNTIME=60 WARNSIG=1 KILLTIME=30 timelimit xvfb-run -s "-screen 0 1400x900x24" python main.py --env-name $ENVIRONMENT --record --start-fresh
-=======
+
 xvfb-run -s "-screen 0 1400x900x24" python main.py --env-name $ENVIRONMENT --record --start-fresh
->>>>>>> f795d89c54c7c6de091a14e7b4a5ba76580e2b3b
 
 git add checkpoints/
 git add playback/
